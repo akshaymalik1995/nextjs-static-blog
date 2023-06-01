af55babd2b079b671cb4ff5300bb3ff1fc81f71e
akshaymalik1995 <akshaymalik191@gmail.com>
2023-06-01 13:49:38 +0530

    Footer at the bottom

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,12 +3,13 @@ import Footer from "./Footer";
 export default function Layout({ children }) {
     return (
         <>
+            <div className="min-h-screen flex flex-col justify-between">
             <Navbar />
-            <main  className="container mx-auto p-5">
+            <main  className="container flex-grow mx-auto p-5">
                 {children}
             </main>
             <Footer />
-            
+            </div>
         </>
     )
 }
