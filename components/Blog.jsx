30067f9c0098b82a331b31a188259d5c7d9d4a7a
akshaymalik1995 <akshaymalik191@gmail.com>
2023-06-07 08:59:05 +0530

    Headings refactored

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,7 +1,7 @@
 
 import Link from 'next/link';
 import Tags from './Tags';
-import { Link as ScrollLink } from 'react-scroll';
+import Headings from './Headings';
 
 export default function Blog({ post, headings }) {
     return (
@@ -22,29 +22,7 @@ export default function Blog({ post, headings }) {
 
             <hr className="my-8" />
 
-            {/* Table of Content */}
-            {headings && headings.length > 1 && (
-                <div className="my-8">
-                    <div className="text-gray-500 text-sm">
-                        <h3 className="text-gray-700 text-lg mb-2">Table of Contents</h3>
-                        <ul className="list-disc list-inside">
-                            {headings.map((heading, index) => (
-                                <li key={index} className={`pl-${heading.level * 2} my-1`}>
-                                    <ScrollLink
-                                        to={heading.id}
-                                        smooth={true}
-                                        duration={500}
-                                        className="hover:text-gray-700 cursor-pointer"
-                                    >
-                                        {heading.text}
-                                    </ScrollLink>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
-            )}
-            {/* End Table of Content */}
+           <Headings headings={headings} />
 
             <article
                 dangerouslySetInnerHTML={{ __html: post.content }}
---
+++ components/Headings.jsx
@@ -0,0 +1,36 @@
+import { Link as ScrollLink } from 'react-scroll';
+export default function Headings({ headings }) {
+    if (!headings || headings.length < 2 ) return null;
+    const headingTabs = {
+        2 : "ml-2",
+        3 : "ml-4",
+        4 : "ml-6",
+        5 : "ml-8",
+        6 : "ml-10",
+    }
+    return (
+        <>
+          
+                <div className="my-8">
+                    <div className="text-gray-500 text-sm">
+                        <h3 className="text-gray-700 text-lg mb-2">Table of Contents</h3>
+                        <div className="">
+                            {headings.map((heading, index) => (
+                                <div key={index} className={`${headingTabs[heading.level]} my-1`}>
+                                    <ScrollLink
+                                        to={heading.id}
+                                        smooth={true}
+                                        duration={500}
+                                        className="hover:text-gray-700 cursor-pointer"
+                                    >
+                                      &bull;  {heading.text}
+                                    </ScrollLink>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+        </>
+
+    )
+}
